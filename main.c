bf5d872a3fba38226eb54a77f2429eb9f2121f87
djam1012 <80195758+djam1012@users.noreply.github.com>
2021-03-07 10:19:34 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,32 @@
+#include "biblioH.h"
+#include "entreeSortieH.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define TAILLE_M 20
+
+int main(int argc, char *argv[]){
+  if (argc!=3){
+    printf("Il faut un seul fichier d'entrée et un seul entier > 0.\n");
+    return 1;
+  }
+  if (atoi(argv[2])==0){
+    printf("Il faut charger au moins une entrée.\n");
+    return 1;
+  }
+  BiblioH* b1 = charger_n_entrees(argv[1], atoi(argv[2]));
+  enregistrer_biblio(b1, "Biblio_rangee.txt");
+
+  /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+  LivreH* l=creer_livre(852, "un_titre", "un_auteur");
+  BiblioH* b=creer_biblio(TAILLE_M);
+  inserer(b, l->num, l->titre, l->auteur); // on crée une copie de ce livre il faut libérer l aussi
+  inserer(b, 4561, "un_autre_titre", l->auteur);
+  inserer(b, 41235, "title", "author");
+
+  liberer_livre(l);
+  liberer_biblio(b);
+
+  return 0;
+}
