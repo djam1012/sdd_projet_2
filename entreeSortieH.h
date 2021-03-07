62b4beabc6b70c317b75c271a1a2186bb63ea400
djam1012 <80195758+djam1012@users.noreply.github.com>
2021-03-07 10:18:06 +0100

    Create entreeSortieH.h

---
+++ entreeSortieH.h
@@ -0,0 +1,8 @@
+#ifndef ENTREESORTIE_2_H
+#define ENTREESORTIE_2_H
+#include "biblioH.h"
+
+BiblioH* charger_n_entrees(char* nomfic, int n);
+void enregistrer_biblio(BiblioH *b, char* nomfic);
+
+#endif
